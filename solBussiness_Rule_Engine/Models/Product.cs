e6b07656ff0a294e24fe55e4d554d9af5b05037f
Sandesh <Sandesh.gundekar@gmail.com>
2020-08-05 10:13:06 +0530

    Models Added

---
+++ solBussiness_Rule_Engine/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        /// <summary>
+        /// 1 - Physical
+        /// 2 - Virtual/Video
+        /// 3 - Membership
+        /// </summary>
+        public Int16 Category { get; set; } //1 - Physical/ 2 - Video/ 3 -Membership
+        /// <summary>
+        /// Type Of Product
+        /// Books 
+        /// If Category is Membership then New or Upgrade
+        /// </summary>
+        public string ProductType { get; set; } //Books /If Membership - New/ Upgrade  
+        
+        public int Agent { get; set; }
+        public double Amount { get; set; }
+    }
+}
